4f859dddbd8b576b92a1ba67edbf2a5d27091c41
Vl4d1sl0veZ4r1p0v <mr.vladislavzaripov@mail.com>
2019-01-21 11:35:26 +0500

    Test

--- client.js
+++ client.js
@@ -1,37 +1,74 @@
-// Создаем текст сообщений для событий
-strings = {
-	'connected': '[sys][time]%time%[/time]: Вы успешно соединились к сервером как [user]%name%[/user].[/sys]',
-	'userJoined': '[sys][time]%time%[/time]: Пользователь [user]%name%[/user] присоединился к чату.[/sys]',
-	'messageSent': '[out][time]%time%[/time]: [user]%name%[/user]: %text%[/out]',
-	'messageReceived': '[in][time]%time%[/time]: [user]%name%[/user]: %text%[/in]',
-	'userSplit': '[sys][time]%time%[/time]: Пользователь [user]%name%[/user] покинул чат.[/sys]'
-};
-window.onload = function() {
-	// Создаем соединение с сервером; websockets почему-то в Хроме не работают, используем xhr
-	if (navigator.userAgent.toLowerCase().indexOf('chrome') != -1) {
-		socket = io.connect('http://localhost:3017', {'transports': ['xhr-polling']});
-	} else {
-		socket = io.connect('http://localhost:3017');
-	}
-	socket.on('connect', function () {
-		socket.on('message', function (msg) {
-			// Добавляем в лог сообщение, заменив время, имя и текст на полученные
-			document.querySelector('#log').innerHTML += strings[msg.event].replace(/\[([a-z]+)\]/g, '<span class="$1">').replace(/\[\/[a-z]+\]/g, '</span>').replace(/\%time\%/, msg.time).replace(/\%name\%/, msg.name).replace(/\%text\%/, unescape(msg.text).replace('<', '&lt;').replace('>', '&gt;')) + '<br>';
-			// Прокручиваем лог в конец
-			document.querySelector('#log').scrollTop = document.querySelector('#log').scrollHeight;
-		});
-		// При нажатии <Enter> или кнопки отправляем текст
-		document.querySelector('#input').onkeypress = function(e) {
-			if (e.which == '13') {
-				// Отправляем содержимое input'а, закодированное в escape-последовательность
-				socket.send(escape(document.querySelector('#input').value));
-				// Очищаем input
-				document.querySelector('#input').value = '';
-			}
-		};
-		document.querySelector('#send').onclick = function() {
-			socket.send(escape(document.querySelector('#input').value));
-			document.querySelector('#input').value = '';
-		};		
+app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+
+var path = require('path');
+
+var port = 3000;
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+
+});
+
+io.on('connection', function(socket){
+	socket.send('Hello, world');//автоматичесски message
+
+	socket.on('greeting', function(data){
+		console.log(data);
 	});
-};
+})
+
+// var rooms = new Map();
+// //var MaxPlayers = 4;
+// io.sockets.on('connection', function (socket) {
+//   //  
+//   let ID = (socket.id).toString().substr(0, 5);
+//     user = {};
+//     user.name = name;
+//     user.ID = ID;
+//     user.room = room;
+//     //
+//     console.log(user);
+//   //
+//   socket.on('SignIn', function(name, room){    
+//     //
+//       if (rooms.has(room)){
+//         rooms[room].add(ID);
+//         rooms[room].array.forEach(element => {
+//           io.sockets.json.send({'event' : 'userJoined', 'name' : ID});
+//         });
+
+//         rooms[room].array.forEach(element => {
+//         console.log({'event' : 'userJoined', 'name' : ID});
+//         });
+        
+//       } else {
+//         rooms.set(room, new Set());
+//         rooms[room].add(user);
+//         //
+//         rooms[room].array.forEach(element => {
+//           console.log({'event' : 'userJoined', 'name' : ID});
+//           });
+//           //
+//       }
+//   })
+
+// 	socket.on('disconnect', function() {
+// 		io.sockets.json.send({'event': 'userSplit', 'name': user.name});
+//   });
+//   rooms[user.room].delete(user);
+//   if (rooms[user.room].size == 0)
+//     rooms.delete(user.room);
+//     //
+//     rooms[room].array.forEach(element => {
+//       console.log({'event' : 'userJoined', 'name' : ID});
+//       });
+//       //
+// });
+
+
+server.listen(port, function(){
+	console.log('app running on port ' + port)
+})

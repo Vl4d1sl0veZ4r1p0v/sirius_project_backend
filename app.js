3d8a2d5a659c9fd3a22ae565bec70c5f20786ca9
Vl4d1sl0veZ4r1p0v <mr.vladislavzaripov@mail.ru>
2019-01-23 06:42:32 +0000

    Last server

--- app.js
+++ app.js
@@ -1,57 +1,64 @@
-//main conf
-var app = require('express')();
+app = require('express')();
 var server = require('http').Server(app);
-var io = require('socket.io').(server); 
+var io = require('socket.io')(server);
+
+var path = require('path');
+
+var port = 80;
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+
+});
 
 var rooms = new Map();
-//var MaxPlayers = 4;
-io.sockets.on('connection', function (socket) {
-  //  
-  let ID = (socket.id).toString().substr(0, 5);
-    user = {};
-    user.name = name;
-    user.ID = ID;
-    user.room = room;
-    //
-    console.log(user);
-	socket.send(user);
-  //
- // socket.on('SignIn', function(name, room){    
-    //
-//      if (rooms.has(room)){
-//        rooms[room].add(ID);
-//        rooms[room].array.forEach(element => {
-//          io.sockets.json.send({'event' : 'userJoined', 'name' : ID});
-//        });
-   
-//        rooms[room].array.forEach(element => {
-//        console.log({'event' : 'userJoined', 'name' : ID});
-//        });
-        
-//      } else {
-//        rooms.set(room, new Set());
-//        rooms[room].add(user);
-        //
-//        rooms[room].array.forEach(element => {
-//          console.log({'event' : 'userJoined', 'name' : ID});
-//          });
-          //
-//      }
-//  })
-//
+
+io.sockets.on('connection', function(socket){				
+	//
+	var user = {};
+	socket.on('SignIn', function(name, room){
+		//		 
+		user.ID = (socket.id).toString().substr(0, 5);;
+		//
+		user.name = data['name'];
+		user.room = data['room'];
+		//
+		console.log(JSON.stringify(user));
+		//
+		if (rooms.has(user.room)){
+			//
+			rooms[user.room].add(user.ID);
+			rooms[user.room].array.forEach(element => {
+				io.sockets.json.send({'event' : 'userJoined', 'name' : user.name});
+			});
+		//
+		rooms[user.room].array.forEach(element => {
+		console.log({'event' : 'userJoined', 'name' : user.name});
+		});
+		
+		} else {
+		rooms.set(user.room, new Set());
+		rooms[user.room].add(user.ID);
+		//
+		rooms[room].array.forEach(element => {
+			console.log({'event' : 'userJoined', 'name' : user.name});
+			});
+		}
+	})
+	//
 //	socket.on('disconnect', function() {
 //		io.sockets.json.send({'event': 'userSplit', 'name': user.name});
-//  });
-//  rooms[user.room].delete(user);
-//  if (rooms[user.room].size == 0)
-//    rooms.delete(user.room);
-    //
-//    rooms[room].array.forEach(element => {
-//      console.log({'event' : 'userJoined', 'name' : ID});
-//      });
-      //
-//});
+//		//
+//		rooms[user.room].delete(user);
+//			if (rooms[user.room].size == 0)
+//				rooms.delete(user.room);
+//		//
+//		rooms[room].array.forEach(element => {
+//			console.log({'event' : 'userJoined', 'name' : ID});
+//		});
+//	});		
+});
 
-app.listen(80, function () {
-    console.log('Example app listening on port 3000!');
-  });
+server.listen(port, function(){
+	console.log('app running on port ' + port)
+})
--- db.js
+++ db.js
@@ -1,1 +1,92 @@
-var Pool = require('pg').Pool;
+app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var path = require('path');
+var port = 80;
+//
+server.unref();
+//
+var mname = '', mtable = '';
+//
+var room = "0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 2 0 0 0 0 0 0 0 0";
+//
+var first = 'Belka', second = 'Kostya';
+var p1 = false, p2 = false;
+//
+io.sockets.on('connection', function(socket){
+	//
+	console.log('connect ', socket.ID);
+	//
+	 socket.on('joinRoom', function(name){//проверяем, что это наши люди
+			//
+			console.log(name, "joined");
+			//
+	 		if (name == first || name == second){
+	 			if (name == first)
+	 				p1 = true;
+	 			else
+	 				p2 = true;
+	 		}
+	 		//
+	 		if (p1 && p2){
+	 			io.sockets.emit('gameStart', function(room){
+					//
+					console.log('gamestart');
+					//
+	 				io.sockets.send(room);
+					 setTimeout(function(){
+					 	io.sockets.emit('finish', function(){
+					 		io.sockets.close();//возможно закрывать можно просто сокет
+					 	})
+					 	console.log('The End.');
+					 }, 3000)
+	 			})
+	 		}
+	 	})
+	// //
+	socket.on('gameTurn', function(name, table){
+		mname = name;
+		mtable = table;
+		//
+		io.sockets.send(mtable);
+		//
+		console.log('gameTurn ' + mname + ' ' + mtable);
+	});
+	//
+	socket.on('finish', function(name){
+		//
+		console.log('finish');
+		//
+		if (name == first || name == second){
+			if (first == name){
+				first = '';
+				p1 = false;
+			} else {
+				second = '';
+				p2 = false;
+			}
+		}
+		if (!(p1 && p2)){
+			io.sockets.send("Finish");
+			io.sockets.close();//возможно закрывать можно просто сокет
+			console.log('Finish');
+		}
+	})
+
+
+	socket.on('disconnect', function(){
+		console.log('disconnect', socket.ID);
+	})
+	//
+	// socket.on('message', function(name, table){
+	// 	mname = name;
+	// 	mtable = table;
+	// 	//
+	// 	io.sockets.send(mtable);
+	// 	console.log(mname + ' ' + mtable);
+	// });
+});
+
+server.listen(port, function(){
+	console.log('app running on port ' + port)
+})
